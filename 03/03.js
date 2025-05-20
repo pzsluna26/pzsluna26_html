f0882f6a1c01710b6fc1efea759736f197a7296c
pzsluna <pzsluna26@gmail.com>
2025-05-21 02:49:00 +0900

    HTML 기초

--- 03/03.js
+++ 03/03.js
@@ -5,6 +5,10 @@ function Hello(){
 */
 
 //화살표 함수(위에 것과 같음)
+/*var변수와 let변수의 차이점
+==> 스코프(유효범위)
+var: 함수 범위를 가짐. 함수 밖에서는 못씀
+let: 블록 범위를 가짐. {} 안에서만 유효함. if,for문 안에서 선언하면 밖에서는 못씀 */
 const Hello = ()=> {
     
     //출력방법1.팝업창: alert("안녕하세요.자바스크립트입니다.!");
@@ -30,13 +34,18 @@ const Hello2 = ()=> {
 const check = () => {
     let s = "123";
     let x = "10";
-    
+    /*"123"문자열
+    "string" 자료형
+    "false(숫자맞음)" 숫자 인지 아닌지 검사*/
 
     console.log(s);
     console.log(typeof(s));
     console.log(isNaN(s));
 
     //inNaN(숫자만포함하냐?) - parseInt(숫자로변환) 숫자인지 아닌지 구분 하고, 연산
+    /*isNaN : 숫자 아니냐? - false숫자맞음 
+    !isNaN : 숫자야? - false숫자아님
+    parseInt : 문자열을 숫자로 바꿔줌 */
     if( !isNaN(s) && !isNaN(x)) {
       //console.log(s+x);  
       s=  parseInt(s);
